aa2cfe96a12e74494b133725a5939a97e7c37d08
masahito <think.p.hm.0203@gmail.com>
2026-07-29 05:02:25 +0900

    変数呼出しの不具合修正

--- liff/script.js
+++ liff/script.js
@@ -505,7 +505,7 @@ function renderReservationCalendar(date, status, capacityData = {}, myReservatio
       });
   }
   // 上限到達時のメッセージ表示
-  if (userLimitReached && !afterInitialRegistration) {
+  if (userLimitReached && !currentUser.afterInitialRegistration) {
     upperLimitMessageArea.classList.remove("hidden");
     if (!userAttendedLimitReached) {
       // upperLimitMessageArea.innerHTML = `<div class='reservedMsg'>今月の予約上限数（${upperLimit}回）に到達しました。</div>`;
@@ -514,7 +514,7 @@ function renderReservationCalendar(date, status, capacityData = {}, myReservatio
       upperLimitMessageArea.innerHTML = `<div class='attendedMsg'>今月の稽古お疲れ様でした🙌</div>`;
     }
   // 初期登録後のメッセージ表示
-  } else if (userLimitReached && afterInitialRegistration) {
+  } else if (userLimitReached && currentUser.afterInitialRegistration) {
     upperLimitMessageArea.innerHTML = `<div class='attendedMsg'>利用情報の登録まで、しばらくお待ちください。</div>`;
   }
 }
