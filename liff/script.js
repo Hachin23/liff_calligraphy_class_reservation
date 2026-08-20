61659b7c48d72059aadca4bd9b6256986f42b802
masahito <think.p.hm.0203@gmail.com>
2026-08-21 00:12:14 +0900

    クリックした座標を元にポップアップ表示場所を決定

--- liff/script.js
+++ liff/script.js
@@ -1081,7 +1081,7 @@ function setupTicketClick(ticketInfo) {
     userPopup.style.display = "none";
     const ticketDetail = ticketInfo.dispInfo
       .map(ticket => `
-      ${ticket.purchaseNumber}回券  残数：${ticket.remainingNumber}/${ticket.purchaseNumber}回<br>
+      ${ticket.purchaseNumber}回券  残数：${ticket.remainingNumber} / ${ticket.purchaseNumber}回<br>
       有効期限：${ticket.expirationDate}
     `)
     .join("<hr>");
@@ -1090,6 +1090,10 @@ function setupTicketClick(ticketInfo) {
       🎫 チケット<br>
       ${ticketDetail}
     `;
+
+    ticketPopup.style.left = `${e.clientX + 10}px`;
+    ticketPopup.style.top = `${e.clientY + 10}px`;
+
     ticketPopup.style.display = "block";
   });
 }
@@ -1134,6 +1138,10 @@ function setupUserClick(currentUser, reservations) {
       予約一覧<br>
       ${userReservationList}
     `;
+
+    userPopup.style.left = `${e.clientX + 10}px`;
+    userPopup.style.top = `${e.clientY + 10}px`;    
+
     userPopup.style.display = "block";
   });
 }
