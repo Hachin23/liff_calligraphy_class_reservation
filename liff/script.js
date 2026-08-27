8af73c4b0926220019df3492d2f2f6c1b3c4b8e1
masahito <think.p.hm.0203@gmail.com>
2026-08-28 08:52:29 +0900

    チケット一覧ポップアップの内容を修正

--- liff/script.js
+++ liff/script.js
@@ -1095,7 +1095,7 @@ function setupTicketClick(ticketInfo) {
     userPopup.style.display = "none";
     const ticketDetail = ticketInfo.dispInfo
       .map(ticket => `
-      ${ticket.purchaseNumber}回券  残数：${ticket.remainingNumber} / ${ticket.purchaseNumber}回<br>
+      残数：${ticket.remainingNumber} / ${ticket.purchaseNumber}回券<br>
       有効期限：${ticket.expirationDate}
     `)
     .join("<hr>");
